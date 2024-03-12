9433b39981c4f4fdedf83d30e297f81f4a43d6e6
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-03-12 18:39:13 +0530

    add setup

--- example_package_saketc/example.py
+++ example_package_saketc/example.py
@@ -1,2 +1,7 @@
-def add_one(number):
-    return number + 1
+import click
+
+@click.command()
+@click.option('--name', default='World', help='Name to greet.')
+def hello(name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    click.echo(f'Hello {name}!')
